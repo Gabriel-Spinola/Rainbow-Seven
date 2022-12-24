093aee141f9705bfd5254bc3f6537d876ce25bfe
gseven-gdv <zerogravitystd@gmail.com>
2022-12-24 15:40:25 -0300

    init project

---
+++ Rainbow Seven/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager s_instance;
+
+    public static InputManager Instance { get => s_instance; }
+
+    public MainInputActions InputActions { private set; get; }
+
+    [Header("Player Movement")]
+    [SerializeField] private Vector2 _moveDir;
+    [SerializeField] private Vector2 _mouseDelta;
+
+    [SerializeField] private bool _playerJumped;
+    
+    public Vector2 MoveDir => _moveDir;
+    public Vector2 MouseDelta => _mouseDelta;
+
+    public bool PlayerJumped => _playerJumped;
+
+    private void Awake()
+    {
+        if (s_instance != null && s_instance != this) {
+            Destroy(this.gameObject);
+        }
+        else {
+            s_instance = this;
+        }
+
+        InputActions = new MainInputActions();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void OnEnable() => InputActions.Enable();
+
+    private void OnDisable() => InputActions.Disable();
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.RightAlt)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (Cursor.lockState is CursorLockMode.Locked && Input.GetKeyDown(KeyCode.LeftAlt)) {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else if (Cursor.lockState is CursorLockMode.None && Input.GetKeyDown(KeyCode.LeftAlt)) {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+#endif
+
+        _moveDir = InputActions.Player.Move.ReadValue<Vector2>();
+        _mouseDelta = InputActions.Player.Look.ReadValue<Vector2>();
+
+        _playerJumped = InputActions.Player.Jump.triggered;
+    }
+}
---
+++ Rainbow Seven/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,51 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float _moveSpeed = 2.0f;
+
+    private CharacterController _controller;
+    private InputManager _inputs;
+    private CinemachineVirtualCamera _cinemachineCamera;
+    private CinemachinePOV _pov;
+
+    private Vector3 _movement;
+    private Vector3 _playerVelocity;
+
+    private bool _isGrounded;
+
+    private void Awake()
+    {
+        _inputs = InputManager.Instance;
+
+        _controller = GetComponent<CharacterController>();
+
+        _cinemachineCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        _pov = _cinemachineCamera.GetCinemachineComponent<CinemachinePOV>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (_isGrounded && _playerVelocity.y < 0f) {
+            _playerVelocity.y = 0f;
+        }
+
+        _movement = new Vector3(_inputs.MoveDir.x, 0f, _inputs.MoveDir.y);
+        _movement = transform.forward * _movement.z + transform.right * _movement.x + transform.up * _playerVelocity.y;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.x, _pov.m_HorizontalAxis.Value, transform.rotation.z);
+
+        _controller.Move(_moveSpeed * Time.deltaTime * _movement);
+        _playerVelocity.y += Physics.gravity.y * Time.deltaTime;
+    }
+}
