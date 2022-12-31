1c4325d945d7f2ab4995e827049894cfb0f99c08
gseven-gdv <zerogravitystd@gmail.com>
2022-12-31 02:34:09 -0300

    Basic UI Placeholder

--- Rainbow Seven/Assets/Scripts/Managers/MainMenu.cs
+++ Rainbow Seven/Assets/Scripts/Managers/MainMenu.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Play()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
